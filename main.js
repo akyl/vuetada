136f3538c2b00328769b8b745f7624e004fb221a
Akyl Zholonov <zholonov@gmail.com>
2019-06-25 02:21:01 +0600

    fix error with websocket

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-var ws = new WebSocket("ws://pm.tada.team/ws")
-ws.onopen = function(event) {
-    console.log('open')
-}
-ws.onmessage = function(event) {
-    console.log(event.data)
-    state = JSON.parse(event.data).state
-}
-
 var app = new Vue({
-    el: '#app',
-    data: {
-        state: true
-    },
-    methods: {
-        changeState: function () {
-            this.state = !this.state
-            var data = {'state': this.state ? 1 : 0}
-            ws.send(JSON.stringify(data))
-        }
+  el: '#app',
+  data: {
+    ws: null,
+    state: true,
+  },
+  created () {
+    this.ws = new WebSocket("ws://pm.tada.team/ws")
+    this.ws.onmessage = function(event) {
+      this.$state = JSON.parse(event.data).state === 1
+      console.log(this.$state)
     }
+  },
+  methods: {
+    changeState: function () {
+      this.state = !this.state
+      var data = {'state': this.state ? 1 : 0}
+      this.ws.send(JSON.stringify(data))
+    }
+  }
 })
 
